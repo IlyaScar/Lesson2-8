ef1326c16992bcadc82a491df6f1e0b3fe8782e6
Markin Pavel <markinpashka@gmail.com>
2026-02-25 08:10:45 +0300

    Comments lesson 2_8

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-
+    /** Эот к какому заданию относится? */
     class Box<T>(val item: T) // Класс
     fun <T> printItem(item: T): T {
         return item
@@ -15,7 +15,7 @@ fun main() {
 
     //task1
     fun <T> validateField(field: T?): Boolean {
-
+        /** Я уже писал про форматирование кода if, надо его придерживаться, а не писать как попало, а еще проще тут вернуть результат сравнения без использования if, return field != null */
         if (field == null) return false
             else (return true)
 
@@ -27,7 +27,7 @@ fun main() {
 
     //task2
     fun <T> countElements(list: List<T>): Int {
-
+        /** Про лишние пустые строки я тоже не раз уже писал, стоит прислушиваться к рекомендациям */
         return list.size
     }
     val testString = listOf("product1","product2","product3")
@@ -37,15 +37,17 @@ fun main() {
     class Basket<T>(val items: List<T>) {
 
         fun addItem(newItem :T) {
-
+            /** Тут реализация не нужна? */
         }
 
         fun getAllItems() {
+            /** Проверял работу этого метода? предполагаю что нет */
             return getAllItems()
         }
 
     }
 //не совсем разобрался что точно тут происходит
+    /** Если не разобрался, как написал? если вопросы лучше приходи с нимми */
     fun <T : Weighable> Basket<T>.totalWeight(): Double {
         return this.items.sumOf { it.weight }
 
@@ -64,12 +66,13 @@ fun main() {
         Book("asd", 233),
         Book("sdfd", 2900)
     ))
-
+    /** Тут вообще не Basket */
     val appleBasket = (listOf(
         Apple("apple1",0.21312,213)
 
     ))
 //тут я немного запутался...разбираюсь
+    /** Доразбирайся */
     var basketTest = Basket<Apple>()
     basketTest.addItem(Apple("new", 23.0, 44))
     println("Veight ${basketTest.totalWeight()}")
@@ -86,7 +89,7 @@ interface Valuable {
     val value: Int
 }
 
-
+/** Оказывается есть второй вариант функции */
 fun <T> validateField(field1: T): Boolean {
 
    return field1!=null
