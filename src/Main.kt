a3bac15d192ade903b99bc28452213544a4bed8f
Ilya Smolin <ilyasmolin9@gmail.com>
2026-03-20 14:06:30 +0300

    Fix

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
     /** Эот к какому заданию относится? */
-    class Box<T>(val item: T) // Класс
-    fun <T> printItem(item: T): T {
-        return item
-    } // Функция
-
-   val test =  printItem(34)
-    println("$test")
-    val test3 = println("testProduct")
-    println(test3)
-
+    //удалил
     //task1
     fun <T> validateField(field: T?): Boolean {
         /** Я уже писал про форматирование кода if, надо его придерживаться, а не писать как попало, а еще проще тут вернуть результат сравнения без использования if, return field != null */
-        if (field == null) return false
-            else (return true)
-
+        // поправил
+        return field != null;
     }
-
     val test5: String = "testProduct"
     println(validateField(test5))
-
 
     //task2
     fun <T> countElements(list: List<T>): Int {
@@ -35,62 +22,52 @@ fun main() {
 
     //task3
     class Basket<T>(val items: List<T>) {
-
+        val listItems = items.toMutableList()
         fun addItem(newItem :T) {
             /** Тут реализация не нужна? */
+            //поправил
+            listItems.add(newItem)
         }
-
-        fun getAllItems() {
+        fun getAllItems( ) : List<T> {
             /** Проверял работу этого метода? предполагаю что нет */
-            return getAllItems()
+            //поправил
+            return listItems
         }
-
     }
 //не совсем разобрался что точно тут происходит
     /** Если не разобрался, как написал? если вопросы лучше приходи с нимми */
     fun <T : Weighable> Basket<T>.totalWeight(): Double {
-        return this.items.sumOf { it.weight }
-
+        return getAllItems().sumOf { it.weight }
     }
-
     fun <T : Valuable> Basket<T>.totalValue(): Int {
-
-        return this.items.sumOf { it.value }
+        return getAllItems().sumOf { it.value }
     }
-
     data class Book(val title:String, override val value: Int) : Valuable
     data class Apple(val sort: String, override val weight: Double, override val value: Int ) : Weighable, Valuable
-
-    val bookBasket = Basket(listOf(
-
-        Book("asd", 233),
-        Book("sdfd", 2900)
+    val bookBasket = Basket<Book>(listOf(
+        Book("Harry Potter 1", 233),
+        Book("Witcher", 2900)
     ))
     /** Тут вообще не Basket */
-    val appleBasket = (listOf(
-        Apple("apple1",0.21312,213)
-
+    val appleBasket = Basket<Apple>(listOf(
+        Apple("Green",0.21312,213),
+        Apple("New Summer",0.24,444)
     ))
 //тут я немного запутался...разбираюсь
     /** Доразбирайся */
-    var basketTest = Basket<Apple>()
-    basketTest.addItem(Apple("new", 23.0, 44))
-    println("Veight ${basketTest.totalWeight()}")
-
-
-
+    appleBasket.addItem(Apple("RedChief", 23.0, 44))
+    println("Apples Weight ${appleBasket.totalWeight()}")
+    println("Apples Value ${appleBasket.totalValue()}")
+    bookBasket.addItem(Book("Harry Potter 2", 23))
+    println("Basket of Books ${bookBasket.totalValue()}")
 }
-
 interface Weighable {
     val weight: Double
 }
-
 interface Valuable {
     val value: Int
 }
-
 /** Оказывается есть второй вариант функции */
-fun <T> validateField(field1: T): Boolean {
-
+/*fun <T> validateField(field1: T): Boolean {
    return field1!=null
-}
+}*/
